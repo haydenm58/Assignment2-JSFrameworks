8f2f4e400b3133db84b96db8f28f56e3d343b823
haydenm58 <hayden.mick58@gmail.com>
2023-12-11 22:04:14 -0700

    useEffect's Cleanup Function

--- Section 11/01-starting-project/01-starting-project/src/components/Modal.jsx
+++ Section 11/01-starting-project/01-starting-project/src/components/Modal.jsx
@@ -1,21 +1,23 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
-
-function Modal({ open, children }) {
+ 
+function Modal({ open, children, onClose }) {
   const dialog = useRef();
-
-  if (open) {
-    dialog.current.showModal();
-  } else {
-    dialog.current.close();
-  }
-
+ 
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+ 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
+    <dialog className="modal" ref={dialog} onClose={onClose}>
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
 }
-
+ 
 export default Modal;
